fe5716e6e0ae3dc8133e073c39825e17f8b328d3
Andrew Akimov <akimov.it.dev@gmail.com>
2025-08-19 21:44:27 +0300

    Added new migration + Removed test databases from the repository

---
+++ migrations/0002_rename_contacts_time_blacklist_change_down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE contacts
+RENAME COLUMN date_blacklist_change TO time_blacklist_change;
---
+++ migrations/0002_rename_contacts_time_blacklist_change_up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE contacts
+RENAME COLUMN time_blacklist_change TO date_blacklist_change;
