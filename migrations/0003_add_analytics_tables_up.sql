0219de98ca5a20436769ce8434fd4d6d52fe0150
Andrew Akimov <akimov.it.dev@gmail.com>
2025-08-24 23:45:37 +0300

    Updated migrations

--- migrations/0003_add_analytics_tables_up.sql
+++ migrations/0003_add_analytics_tables_up.sql
@@ -1,22 +1,13 @@
-CREATE TABLE vacancy_analytics_groups (
+CREATE TABLE vacancy_analytics_hedalines (
   "id" TEXT NOT NULL,
   "name" TEXT NOT NULL UNIQUE,
+  "lang" TEXT,
+  "sphere" TEXT,
+  "stack" TEXT,
   "is_active" INTEGER NOT NULL DEFAULT 1,
 	"time_create"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"time_edit"	TEXT,
   PRIMARY KEY("id")
-);
-
-CREATE TABLE vacancy_analytics_hedalines (
-  "id" TEXT NOT NULL,
-  "lang" TEXT NOT NULL,
-  "group_id" TEXT,
-  "name" TEXT NOT NULL UNIQUE,
-  "is_active" INTEGER NOT NULL DEFAULT 1,
-	"time_create"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	"time_edit"	TEXT,
-  PRIMARY KEY("id"),
-  FOREIGN KEY("group_id") REFERENCES "vacancy_analytics_groups"("id") ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE vacancy_analytics (
