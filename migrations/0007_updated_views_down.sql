88a53b8f33c88598d556363a05966af03a8e5ab7
Andrew Akimov <akimov.it.dev@gmail.com>
2025-08-30 06:51:24 +0300

    Added a migration — Updated views

---
+++ migrations/0007_updated_views_down.sql
@@ -0,0 +1,21 @@
+DROP VIEW vacancy_stats_daily;
+
+CREATE VIEW vacancy_stats_daily AS
+SELECT
+	vah.id AS headline_id,
+	vah.name AS headline_name,
+	vasp.id AS sphere_id,
+	vasp.name AS sphere_name,
+	vast.id AS stack_id,
+	vast.name AS stack_name,
+	vasrc.id AS source_id,
+	AVG(va.amount) as avg_amount,
+	va.date_check AS date_check
+FROM vacancy_analytics_headlines AS vah
+LEFT JOIN vacancy_analytics AS va ON va.headline_id = vah.id
+LEFT JOIN vacancy_analytics_sources AS vasrc ON vasrc.id = va.source_id
+LEFT JOIN vacancy_analytics_spheres AS vasp ON vasp.id = vah.sphere_id
+LEFT JOIN vacancy_analytics_stack AS vast ON vast.id = vah.stack_id
+WHERE vah.is_active = 1 AND vasrc.is_active = 1
+GROUP BY date_check, sphere_id, stack_id
+ORDER BY headline_id, date_check;
