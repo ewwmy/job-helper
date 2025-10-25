74dffb75c06ae2bb45bf888576b196e34fae7e9e
Andrew Akimov <akimov.it.dev@gmail.com>
2025-10-25 07:34:22 +0300

    Updated latest vacancy dynamics view

---
+++ migrations/0022_update_view_vacancy_dynamics_latest_down.sql
@@ -0,0 +1,15 @@
+DROP VIEW "vacancy_dynamics_latest";
+
+CREATE VIEW "vacancy_dynamics_latest" AS
+SELECT
+    headline_name,
+    sphere_name,
+    stack_name,
+    MAX(date_check),
+    avg_amount,
+    abs_change,
+    pct_change,
+    base_abs_change,
+    base_pct_change
+FROM vacancy_dynamics
+GROUP BY headline_name;
